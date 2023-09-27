a21f6075f1fd244b23a1233dbf8ec08ebd5a0a8b
roal518 <roalqwerty@gmail.com>
2023-09-28 02:01:57 +0900

    캐릭터가 마우스 포인터 찍은 곳으로 이동한다.

--- move_to_mouse.py
+++ move_to_mouse.py
@@ -9,13 +9,16 @@ Arrow_cursur = load_image('hand_arrow.png')
 
 running = True
 moving = False
-mouseX = 0
-mouseY = 0
+mouseX = TUK_WIDTH//2
+mouseY = TUK_HEIGHT//2
+nowX, nowY = 0, 0
+x, y = 0, 0
 isClicked = False
 # handle_events--> keyboard // mouse
 def handle_events():
     global running, moving
     global mouseX, mouseY, isClicked
+    global nowX,nowY
     events = get_events()
     for event in events:
         if event.type == SDL_KEYDOWN:
@@ -24,18 +27,34 @@ def handle_events():
         elif event.type == SDL_MOUSEBUTTONDOWN:
             if event.button == 1:
                 isClicked = True
-                move_character()
+                nowX, nowY = mouseX, mouseY
                 mouseX, mouseY = event.x, TUK_HEIGHT - 1 - event.y
+                move_character()
         elif event.type == SDL_MOUSEBUTTONUP:
             if event.button == 1:
                 isClicked = False
 def move_character():
-    pass
+    global x, y, nowX, nowY, mouseX, mouseY, i
+    t = i / 100
+    x = (1 - t) * nowX + t * mouseX
+    y = (1 - t) * nowY + t * mouseY
+    IDLE_character.clip_draw(idleframe * 100, 0, 100, 100, x, y)
+
+
 idleframe = 0
+i = 0
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
+    Arrow_cursur.draw(mouseX, mouseY)
+    if(i < 100):
+        move_character()
+        i += 1
+    elif(i == 100):
+        handle_events()
+        i = 0
     update_canvas()
-    handle_events()
+
+    idleframe = (idleframe + 1) % 4
     delay(0.05)
 close_canvas()
