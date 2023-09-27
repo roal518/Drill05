a14b5c7dadf05034356f7e9c6dcfada56ab0e544
roal518 <roalqwerty@gmail.com>
2023-09-28 01:31:43 +0900

    get mouse click
    사진 가로-세로 비가 안 맞는지 원점이 0,120에 맞춰져 있다고 생각하는 것이 좋을거같다.

--- move_to_mouse.py
+++ move_to_mouse.py
@@ -1,20 +1,21 @@
 from pico2d import *
-import math
 
 TUK_WIDTH, TUK_HEIGHT = 1280,1024
 open_canvas(TUK_WIDTH,TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 IDLE_character = load_image('Woodcutter_idle.png')
 MOVE_character = load_image('Woodcutter_run.png')
-Arrow_cursur = load_image('hand_arrow')
+Arrow_cursur = load_image('hand_arrow.png')
 
 running = True
 moving = False
-xdir = 0
-ydir = 0
+mouseX = 0
+mouseY = 0
+isClicked = False
+# handle_events--> keyboard // mouse
 def handle_events():
     global running,moving
-    global xdir, ydir
+    global mouseX, mouseY, isClicked
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT: # 창 닫기가 없긴 없다. ESC로 닫자
@@ -22,13 +23,22 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 running = False
-
-runframe = 0
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == 1:
+                isClicked = True
+                move_character()
+                mouseX,mouseY = event.x,TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONUP:
+            if event.button ==1:
+                isClicked = False
+def move_character():
+    pass
 idleframe = 0
+
 while running:
     clear_canvas()
-    tuk_ground.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
+    tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
     update_canvas()
     handle_events()
-
+    delay(0.05)
 close_canvas()
