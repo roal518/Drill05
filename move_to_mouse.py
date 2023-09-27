cd8afa3f9255f08b9e456b70e606190ee31732ba
roal518 <roalqwerty@gmail.com>
2023-09-28 00:29:00 +0900

    make basic setting

--- move_to_mouse.py
+++ move_to_mouse.py
@@ -1,1 +1,30 @@
-1
+from pico2d import *
+import math
+
+TUK_WIDTH, TUK_HEIGHT = 1280,1024
+open_canvas(TUK_WIDTH,TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+IDLE_character = load_image('Woodcutter_idle.png')
+MOVE_character = load_image('Woodcutter_run.png')
+
+running = True
+moving = False
+xdir = 0
+ydir = 0
+def handle_events():
+    global running,moving
+    global xdir, ydir
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT: # 창 닫기가 없긴 없다. ESC로 닫자
+            running = False
+
+runframe = 0
+idleframe = 0
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
+    update_canvas()
+    handle_events()
+
+close_canvas()
