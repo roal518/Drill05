c5d724e3f555067cc931291d4d73613d32b143d2
roal518 <roalqwerty@gmail.com>
2023-09-28 02:30:12 +0900

    오류 수정
    마우스 클릭 이후 캐릭터가 원래 점으로 다시 돌아가서 애니메이션을 반복하는 버그를 수정했다.

--- move_to_mouse.py
+++ move_to_mouse.py
@@ -12,49 +12,65 @@ moving = False
 mouseX = TUK_WIDTH//2
 mouseY = TUK_HEIGHT//2
 nowX, nowY = 0, 0
-x, y = 0, 0
+x, y = mouseX, mouseY
 isClicked = False
 # handle_events--> keyboard // mouse
+def keyboard_events():
+    global running
+    keyevents = get_events()
+    for keyevent in get_events():
+        if keyevent.type == SDL_KEYDOWN:
+            if keyevent.key == SDLK_ESCAPE:
+                running = False
+
+
 def handle_events():
-    global running, moving
     global mouseX, mouseY, isClicked
     global nowX,nowY
+    global i
     events = get_events()
     for event in events:
-        if event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_MOUSEBUTTONDOWN:
+        if event.type == SDL_MOUSEBUTTONDOWN:
             if event.button == 1:
                 isClicked = True
                 nowX, nowY = mouseX, mouseY
                 mouseX, mouseY = event.x, TUK_HEIGHT - 1 - event.y
                 move_character()
+                i = 0
         elif event.type == SDL_MOUSEBUTTONUP:
             if event.button == 1:
                 isClicked = False
+
 def move_character():
     global x, y, nowX, nowY, mouseX, mouseY, i
-    t = i / 100
-    x = (1 - t) * nowX + t * mouseX
-    y = (1 - t) * nowY + t * mouseY
-    IDLE_character.clip_draw(idleframe * 100, 0, 100, 100, x, y)
-
+    global moving
+    if(x == mouseX and y == mouseY):
+        moving = False
+    elif (x != mouseX and y != mouseY):
+        moving = True
+        t = i / 100
+        x = (1 - t) * nowX + t * mouseX
+        y = (1 - t) * nowY + t * mouseY
+        IDLE_character.clip_draw(idleframe * 100, 0, 100, 100, x, y)
 
 idleframe = 0
 i = 0
+index = 0
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
     Arrow_cursur.draw(mouseX, mouseY)
-    if(i < 100):
+    if(moving):
         move_character()
         i += 1
-    elif(i == 100):
-        handle_events()
-        i = 0
-    update_canvas()
+    else:
+        IDLE_character.clip_draw(idleframe * 100, 0, 100, 100, x, y)
 
+    handle_events()
+
+    keyboard_events()
+    update_canvas()
     idleframe = (idleframe + 1) % 4
     delay(0.05)
+
 close_canvas()
