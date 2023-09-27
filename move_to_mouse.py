72e61bef5aefa2c57a48a1e872700cff2871b3e1
roal518 <roalqwerty@gmail.com>
2023-09-28 01:35:44 +0900

    shutdown only use ESC

--- move_to_mouse.py
+++ move_to_mouse.py
@@ -1,7 +1,7 @@
 from pico2d import *
 
-TUK_WIDTH, TUK_HEIGHT = 1280,1024
-open_canvas(TUK_WIDTH,TUK_HEIGHT)
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 IDLE_character = load_image('Woodcutter_idle.png')
 MOVE_character = load_image('Woodcutter_run.png')
@@ -14,27 +14,24 @@ mouseY = 0
 isClicked = False
 # handle_events--> keyboard // mouse
 def handle_events():
-    global running,moving
+    global running, moving
     global mouseX, mouseY, isClicked
     events = get_events()
     for event in events:
-        if event.type == SDL_QUIT: # 창 닫기가 없긴 없다. ESC로 닫자
-            running = False
-        elif event.type == SDL_KEYDOWN:
+        if event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
             if event.button == 1:
                 isClicked = True
                 move_character()
-                mouseX,mouseY = event.x,TUK_HEIGHT - 1 - event.y
+                mouseX, mouseY = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONUP:
-            if event.button ==1:
+            if event.button == 1:
                 isClicked = False
 def move_character():
     pass
 idleframe = 0
-
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
